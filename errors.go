4274b4dc40db363f1447fe14ab01935f1886e494
Derek Smith <derek.smith@naveego.com>
2016-08-04 12:18:49 -0400

    Initial commit

---
+++ errors.go
@@ -0,0 +1,33 @@
+package types
+
+// Error is the main error type that will be used by components of the
+// pipeline.  It adds a machine readable code that can be used to deteremine
+// the type of error.
+type Error struct {
+	Code    int    // The machine readable error code
+	Message string // The human readable error message
+}
+
+// Error implements the native golang error interface
+func (e Error) Error() string {
+	return e.Message
+}
+
+// HTTPStatus returns the HTTP status code for the error
+func (e Error) HTTPStatus() int {
+	if e.Code >= 4000000 {
+		return e.Code / 10000
+	}
+
+	return 500
+}
+
+// New implements the built-in errors.New method
+func New(message string) error {
+	return Error{Code: 5000000, Message: message}
+}
+
+// NewWithCode returns a error with the given code
+func NewWithCode(code int, message string) error {
+	return Error{Code: code, Message: message}
+}
---
+++ errors_test.go
@@ -0,0 +1,90 @@
+package types
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestNew(t *testing.T) {
+
+	Convey("Calling errors.New('My Error Message')", t, func() {
+
+		err := New("My Error Message")
+
+		Convey("Should return an error of type errors.Error", func() {
+			_, ok := err.(Error)
+			So(ok, ShouldBeTrue)
+		})
+
+		Convey("Should set the message to 'My Error Message'", func() {
+			So(err.Error(), ShouldEqual, "My Error Message")
+		})
+
+		Convey("Shuld set the default error code to 5000000", func() {
+			e, _ := err.(Error)
+			So(e.Code, ShouldEqual, 5000000)
+		})
+
+	})
+}
+
+func TestNewWithCode(t *testing.T) {
+
+	Convey("Calling errors.NewWithCode(400001, 'My Error Message')", t, func() {
+
+		err := NewWithCode(200001, "My Error Message")
+
+		Convey("Should return an error of type errors.Error", func() {
+			_, ok := err.(Error)
+			So(ok, ShouldBeTrue)
+		})
+
+		Convey("Should set the message to 'My Error Message'", func() {
+			So(err.Error(), ShouldEqual, "My Error Message")
+		})
+
+		Convey("Shuld set the default error code to 200001", func() {
+			e, _ := err.(Error)
+			So(e.Code, ShouldEqual, 200001)
+		})
+
+	})
+
+}
+
+func TestHTTPStatus(t *testing.T) {
+
+	Convey("An error with code less than 400000", t, func() {
+		Convey("Should return HTTP StStatusauts 500", func() {
+			err := &Error{Code: 300, Message: "Test"}
+			So(err.HTTPStatus(), ShouldEqual, 500)
+			err = &Error{Code: 100, Message: "Test"}
+			So(err.HTTPStatus(), ShouldEqual, 500)
+			err = &Error{Code: 300001, Message: "Test"}
+			So(err.HTTPStatus(), ShouldEqual, 500)
+		})
+	})
+
+	Convey("An error with code greater than 4000000", t, func() {
+		Convey("Should return the HTTP Status identified by the first 3 digits", func() {
+
+			Convey("So an error code of 4000005 should return 400", func() {
+				err := &Error{Code: 4000005, Message: "Test"}
+				So(err.HTTPStatus(), ShouldEqual, 400)
+			})
+
+			Convey("So an error code of 4220005 should return 422", func() {
+				err := &Error{Code: 4220005, Message: "Test"}
+				So(err.HTTPStatus(), ShouldEqual, 422)
+			})
+
+			Convey("So an error code of 4150005 should return 415", func() {
+				err := &Error{Code: 4150005, Message: "Test"}
+				So(err.HTTPStatus(), ShouldEqual, 415)
+			})
+
+		})
+	})
+
+}
