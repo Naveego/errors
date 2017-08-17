31819ff2f0c0c4e7a729100bc7334d1962b75150
AzureAD\SteveRuble <steve.ruble@naveego.com>
2017-08-17 15:59:50 -0400

    Added DebugMessage to Error.

--- errors.go
+++ errors.go
@@ -6,8 +6,13 @@ import "fmt"
 // pipeline.  It adds a machine readable code that can be used to deteremine
 // the type of error.
 type Error struct {
-	Code    int    // The machine readable error code
-	Message string // The human readable error message
+	// The machine readable error code
+	Code int
+	// The human readable error message
+	Message string
+	// DebugMessage is a message which will be
+	// logged but never sent to the user
+	DebugMessage string
 }
 
 // Error implements the native golang error interface
@@ -27,6 +32,11 @@ func (e Error) HTTPStatus() int {
 // New implements the built-in errors.New method
 func New(message string) error {
 	return Error{Code: 5000000, Message: message}
+}
+
+// NewWithDebug returns a new error with a user message and a debug-only message.
+func NewWithDebug(message, debugMessag string) error {
+	return Error{Code: 5000000, Message: message, DebugMessage: debugMessag}
 }
 
 // NewWithCode returns a error with the given code
